e93e1b94711130badfd0a2432cf551de543c474d
Shashikanth Reddy Palvatla <25389431+shashikanth171@users.noreply.github.com>
2018-01-06 22:52:29 +0530

    Added Indicator Stickynotes

--- install_programs.sh
+++ install_programs.sh
@@ -171,3 +171,6 @@ curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 sudo apt update && sudo apt install signal-desktop
 
+#21 Indicator Stickynotes
+echo | sudo apt-add-repository ppa:umang/indicator-stickynotes
+sudo apt-get update && sudo apt-get install indicator-stickynotes
